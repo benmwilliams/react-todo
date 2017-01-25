416b52a9d20fe60288d22cb76f65ae5bfa697b46
Ben Williams <benwilliamsdev@gmail.com>
2017-01-24 19:25:38 -0500

    adds lodash and express

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import { render } from "react-dom";
-import React from "react";
-import App from "./components/App";
+import { render } from "react-dom"
+import React from "react"
+import App from "./components/App"
 
-const containerEl = document.getElementById("container");
+const containerEl = document.getElementById("container")
 
 render(
 	<App/>,
